9e26693bd3987300d2e87842a1c3c60f691c15c3
tsukaryu <drgon8otsuka@gmail.com>
2024-03-16 00:37:41 +0900

    ME: add createApp API

--- packages/runtime-dom/index.ts
+++ packages/runtime-dom/index.ts
@@ -1,4 +1,17 @@
+import { CreateAppFunction, createAppAPI } from "../runtime-core/apiCreateApp";
 import { createRenderer } from "../runtime-core/renderer";
 import { nodeOps } from "./nodeOps";
 
 const { render } = createRenderer(nodeOps);
+const _createApp = createAppAPI(render);
+
+export const createApp = ((...args) => {
+  const app = _createApp(...args);
+  const { mount } = app;
+  app.mount = (selector: string) => {
+    const container = document.querySelector(selector);
+    if (!container) return;
+    mount(container);
+  };
+  return app;
+}) as CreateAppFunction<Element>;
